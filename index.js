f705952ac14c9c5078fe780d611e34bbabde177a
bidex327 <adebayobidemi303@gmail.com>
2026-07-19 15:38:55 +0100

    Updated

--- index.js
+++ index.js
@@ -1,80 +1,3 @@
-// const express = require("express");
-// const app = express();
-// require("dotenv").config();
-// const cors = require("cors");
-// const userRoutes = require("./routes/userRoutes");
-// const cardsRoutes = require("./routes/cards")
-
-
-// const { dbConnect } = require("./lib/dbConnect");
-// const connectDB = async (req, res, next) => {
-//   try {
-//     await dbConnect();
-//     next();
-//   } catch (error) {
-//     console.error("Database connection failed:", error);
-//     res
-//     .status(503)
-//     .json({
-//       message: "Service Unavailable: Could not connect to the database.",
-//     });
-//   }
-// };
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-
-// app.use(cors({
-//   origin: [
-//     "http://localhost:5174", // local dev
-//     "https://gensler-i4tt.vercel.app" // production frontend
-//   ],
-//   methods: ["GET","POST","PUT","DELETE"],
-//   credentials: true
-// }));
-
-
-// dbConnect();
-
-
-
-// app.use("/api",  userRoutes,);
-// app.use("/api", cardsRoutes)
-
-// app.get("/api/cards", (req, res) => {
-  
-//   res.status(200).json(cards);
-// });
-// app.get("/", (req, res) => {
-//   res.send("welcome to my Api");
-// });
-
-// if (process.env.NODE_ENV !== "production") {
-//   const PORT = 5000;
-//   app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-//   });
-// }
-
-// module.exports = app;
-
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, x-Requested-with Content-Type,Accept, Authorization;"
-//   );
-//   next();
-
-// });
-
-// app.use(cors({
-//   origin: "#",
-//   methods: ["GET", "POST", "PUT", "DELETE"]
-// }));
-
-
 const express = require("express");
 const app = express();
 require("dotenv").config();
@@ -87,7 +10,6 @@ const { dbConnect } = require("./lib/dbConnect");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Connect once at startup
 (async () => {
   try {
     await dbConnect();
@@ -100,15 +22,14 @@ app.use(
   cors({
     origin: [
       "http://localhost:5174",
-       "https://gensler-aamb.vercel.app",
-       "https://gensler-m53g.vercel.app",
-       "https://gensler-dpx7.vercel.app",
+      "https://gensler-aamb.vercel.app",
+      "https://gensler-m53g.vercel.app",
+      "https://gensler-dpx7.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-  })
+    credentials: true,
+  }),
 );
-
 
 app.use("/api", userRoutes);
 app.use("/api", cardsRoutes);
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,24 +1,26 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/userSchema'); // Assuming you want to attach the user object to req
-require('dotenv').config()
-const JWT_SECRET = process.env.SECRET_KEY; // Use the same key as in generateToken
+const jwt = require("jsonwebtoken");
+const User = require("../models/userSchema");
+require("dotenv").config();
+const JWT_SECRET = process.env.SECRET_KEY;
 const protect = async (req, res, next) => {
   let token;
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
     try {
-      // Get token from header
-      token = req.headers.authorization.split(' ')[1];
-      // Verifing my token
+      token = req.headers.authorization.split(" ")[1];
+
       const decoded = jwt.verify(token, JWT_SECRET);
-      req.user = await User.findById(decoded.id).select('password');
+      req.user = await User.findById(decoded.id).select("password");
       next();
     } catch (error) {
-      console.error('Token verification failed:', error.message);
-      return res.status(401).json({ message: 'Not authorized, token failed' });
+      console.error("Token verification failed:", error.message);
+      return res.status(401).json({ message: "Not authorized, token failed" });
     }
   }
   if (!token) {
-    return res.status(401).json({ message: 'Not authorized, no token' });
+    return res.status(401).json({ message: "Not authorized, no token" });
   }
 };
 module.exports = { protect };
