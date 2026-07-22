d2b582da8f08fab997f2e7b93e05b2cf366f5050
bidex327 <adebayobidemi303@gmail.com>
2026-07-22 03:33:57 +0100

    Update

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get("/", (req, res) => res.send("Welcome to my API"));
 
 if (process.env.NODE_ENV !== "production") {
   const PORT = 5000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 }
 
 module.exports = app;
